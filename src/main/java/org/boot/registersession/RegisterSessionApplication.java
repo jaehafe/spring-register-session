89a055b12edc4c856fdb38f669ba9303b7d08f75
Jaeha Lee <wogkdkrm112@gmail.com>
2024-06-02 23:53:31 +0900

    init: project init, db Setting

---
+++ src/main/java/org/boot/registersession/RegisterSessionApplication.java
@@ -0,0 +1,13 @@
+package org.boot.registersession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterSessionApplication.class, args);
+    }
+
+}
